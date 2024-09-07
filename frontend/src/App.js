d31bc97090a13703e0817d493df231155a4d72e5
Bianco Zeng <z5359389@ad.unsw.edu.au>
2024-09-08 01:35:30 +1000

    routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,13 +4,14 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import PageList from './PageList';
 import { ThemeContext } from './ThemeContext';
 import Landing from './Landing';
+import Dashboard from './Dashboard';
 
 function App() {
   return (
     <ThemeContext>
-      <Router>
-        <PageList />
-      </Router>
+      {/* <Router> */}
+      <Dashboard />
+      {/* </Router> */}
       {/* <Landing /> */}
     </ThemeContext>
   );
